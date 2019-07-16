1e92d618ca57e11b9e313baeae02fe3b44d5e42d
Arjun <arjun99.chauhan@gmail.com>
2019-07-16 14:48:21 +0530

    Initial workflow setup, HTML markup, menu, menu button, js file

---
+++ dist/js/main.js
@@ -0,0 +1,27 @@
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const navLinks = document.querySelectorAll(".nav-link");
+
+let showMenu = false;
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    navLinks.forEach(item => item.classList.add("show"));
+
+    // Set Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    navLinks.forEach(item => item.classList.remove("show"));
+
+    // Set Menu State
+    showMenu = false;
+  }
+}
